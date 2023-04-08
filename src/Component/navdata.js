31808e0863ea5cedce9f671f540e9aa6f4e424d6
rahul rawat <golubook09@gmail.com>
2023-04-08 19:19:38 +0100

    remove hard code links and make linke sprate file and import this file in navbar page

---
+++ src/Component/navdata.js
@@ -0,0 +1,88 @@
+export const links= [
+    {
+        img:'https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg',
+        p:'frams'
+    }
+    ,
+    {
+        img:'https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg',
+        p:'Lakefront'
+    },
+    {
+        img:'https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg',
+        p:'Breakfast'
+    },
+    {
+        img:'https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg',
+        p:'Mainsions'
+    },
+   
+]
+
+ 
+    
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>frams</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Lakefront</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg
+// ' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Breakfast</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg
+// ' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Mainsions</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/10ce1091-c854-40f3-a2fb-defc2995bcaf.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Beach</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/35919456-df89-4024-ad50-5fcb7a472df9.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Tiny Homes</p>
+// </div>
+
+// <div>
+// <img src='
+// https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Private Room</p>
+// </div>
+
+
+// <div>
+// <img src=' https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>OMG</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Country Side</p>
+// </div>
+
+// <div>
+// <img src='https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg
+// ' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}>Historical Homes</p>
+// </div>
+
+// <div>
+// <img src=' https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg' alt='img' style={{ height: '25px', width: '25px' }}  />
+// <p style={{marginTop:'2px'}}> Cabins</p>
+// </div>
+
+// <div>
+// {<img src={filterr} alt='img' /* style={{ height: '25px', width: '25px' }}*/  /> }
+
+// </div>
