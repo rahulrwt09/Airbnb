93995c15ce2955a347ebbe03077db268c8048832
rahul rawat <golubook09@gmail.com>
2023-04-08 11:08:32 +0100

    create components header,nav main

---
+++ src/Component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <div className='header'>
+        <div>Logo</div>
+        <div>search</div>
+        <div>your profile</div>
+    </div>
+    </>
+
+  )
+}
+
+export default Header
