9fb99759fdd1fce53cdf3dac2b93fa6e81192615
rahul rawat <golubook09@gmail.com>
2023-04-08 12:18:06 +0100

    create a header

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
        <Header/>
-       <Main/>
-       <Navbar/>
+       {/* <Main/>
+       <Navbar/> */}
     </div>
   );
 }
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import logo from './img/logo.PNG';
+import logomiddle from './img/middle.PNG'
+import lastlogo from './img/lastlogo.PNG'
+const Header =()=>{
+//-------------- css here-----------------------------------//
 
-function Header() {
+const head= {
+    border:'1px solid black',
+
+     display:'flex',
+     flexDirection:'row',
+     justifyContent:'space-around',
+     height:'10vh',
+     alignItems: 'center',
+}
+const logofirst={
+      
+}
+
+//-------------------css------------------------------------//
   return (
     <>
-    <div className='header'>
-        <div>Logo</div>
-        <div>search</div>
-        <div>your profile</div>
+    <div style={head} >
+
+
+        <div style={logofirst}>
+         <img src={logo} alt={logo}/>
+        </div>
+        
+
+        <div >
+        <img src={logomiddle} alt={logomiddle} />   
+        </div>
+
+
+          <div>
+          <img src={lastlogo } alt={lastlogo } />
+        </div>
     </div>
     </>
 
