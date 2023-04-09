4316c2d366fe511ab7eb6d158c2f1c0a852eb7b2
rahul rawat <golubook09@gmail.com>
2023-04-09 18:09:10 +0100

    add tempory grid in fetch data

--- src/Main.js
+++ src/Main.js
@@ -8,13 +8,33 @@ function Main() {
      .then(data=>setdata(data))
      .catch(err=>console.error(err))
   },[])
+  const css= {
+   border:'1px solid balck',
+   display: 'grid',
+   gridTemplateColumns: 'repeat(4, 1fr)',
+   gridTemplateRows: 'repeat(4, 1fr)',
+   gridGap: '7px',
+   paddingLeft:'20px',
+   paddingTop:'20px'
+  }
+  const subcss={
+    height:'250px',
+    width:'280px',
+    
+  }
+  const imgcss= {
+  backgroundSize:'cover',
+  width: '100%',
+  height:' 100%',
+  borderRadius:'5%',
+  }
   console.log(data);
   return (
     <>
-    <div >
+    <div style={css} >
     {data.map(el=>(
-     <div>
-      <img src={el.image}/>
+     <div style={subcss}>
+      <img src={el.image} style={imgcss}/>
       <p>{el.name}</p>
       <p>{el.location}</p>
       <p>{el.rating }</p>
