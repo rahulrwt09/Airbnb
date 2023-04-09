aa16fe689151841ad1582f04ba3f89c195f4e495
rahul rawat <golubook09@gmail.com>
2023-04-09 18:43:20 +0100

    add fontosm icon in page

--- src/Main.js
+++ src/Main.js
@@ -32,13 +32,19 @@ function Main() {
   console.log(data);
   return (
     <>
+    <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    </head>
     <div style={css} >
     {data.map(el=>(
      <div style={subcss}>
       <img src={el.image} style={imgcss}/>
-      <p>{el.name}</p>
-      <p>{el.location}</p>
-      <p>{el.rating }</p>
+      <p>{<i class="fa-solid fa-hotel" style={{color:'#FF5A5F'}}></i>}{"-"}{el.name}</p>
+      
+      <p>{<i class="fa-solid fa-location-dot" style={{color:'red'}}></i>}{" "}{el.location}</p>
+      <p>{<i class="fa-solid fa-star" style={{color:'#e3ac07'}}></i>}{" "}{el.rating }</p>
       
 
      </div>
